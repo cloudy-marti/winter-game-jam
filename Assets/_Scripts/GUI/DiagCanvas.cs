3bb83617177468a2add69653333e4862b5f977ba
LeducBastien <leduc.bastien.info@gmail.com>
2017-12-15 22:51:16 +0100

    Dialogue prefab, DOTween

---
+++ Assets/_Scripts/GUI/DiagCanvas.cs
@@ -0,0 +1,17 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace _Scripts.GUI
+{
+	public class DiagCanvas : MonoBehaviour
+	{
+		private CanvasGroup _canvasGroup;
+
+		// Use this for initialization
+		void Start ()
+		{
+			_canvasGroup = GetComponent<CanvasGroup>();
+			_canvasGroup.DOFade(1f, 1f);
+		}
+	}
+}
